9f9f3f23508fe6269d7a905f4985db7ad72637ff
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-05-11 14:44:04 +0100

    Start of a slurm driver, providing array-based batching of multiple jobs.

---
+++ phdscripts/scheduler/slurm/driver.py
@@ -0,0 +1,17 @@
+"""
+Functions for interacting with Slurm scheduler.
+"""
+
+from os import popen
+
+
+def batch_jobs(job_script: str, job_count: int, jobs_parallel: int = 1):
+    cmd = f"sbatch --array=0-{job_count-1}"
+
+    if jobs_parallel > 0:
+        cmd += f"%{jobs_parallel}"
+
+    cmd += f" {job_script}"
+
+    # TODO(Matthew): Do we want to capture any output?
+    popen(cmd)
