510e643ef20001dcf825408825eb06c29b92b21e
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-05-13 10:29:14 +0100

    Export const symbols.

--- phdscripts/workflows/__init__.py
+++ phdscripts/workflows/__init__.py
@@ -1,4 +1,15 @@
 from .register import WorkflowRegister
-from .workflow import Workflow, WorkflowSettings
+from .workflow import (
+    JOB_SCRIPT_FILENAME,
+    PARAM_SET_REGISTER_FILENAME,
+    Workflow,
+    WorkflowSettings,
+)
 
-__all__ = ["Workflow", "WorkflowSettings", "WorkflowRegister"]
+__all__ = [
+    "JOB_SCRIPT_FILENAME",
+    "PARAM_SET_REGISTER_FILENAME",
+    "Workflow",
+    "WorkflowSettings",
+    "WorkflowRegister",
+]
