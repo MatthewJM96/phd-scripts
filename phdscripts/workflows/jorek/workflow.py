6918169b286f6dfa357376a8b0f78acb67fe71ab
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-05-13 16:52:12 +0100

    Need to use re.search not re.match for JOREK input.

--- phdscripts/workflows/jorek/workflow.py
+++ phdscripts/workflows/jorek/workflow.py
@@ -133,7 +133,7 @@ mpirun -n 2                                                     \\
             value_str = str(value)
             value_str = re.sub(r"([0-9]+).d([0-9]+)", r"\1.e\2", value_str)
 
-            if re.match(rf"{param} *= *-?[0-9]+[.d?[0-9]*]?", jorek_input) is not None:
+            if re.search(rf"{param} *= *-?[0-9]+[.d?[0-9]*]?", jorek_input) is not None:
                 jorek_input = re.sub(
                     rf"{param} *= *-?[0-9]+[.d?[0-9]*]?",
                     f"{param} = {value_str}",
