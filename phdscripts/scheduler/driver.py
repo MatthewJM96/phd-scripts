16670511643efa16d3e85f5758c861ae81b99301
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-05-17 13:32:31 +0100

    Allow for a dependency to be specified in which each job in an array depends on the equivalent-index job in another job array.

--- phdscripts/scheduler/driver.py
+++ phdscripts/scheduler/driver.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractclassmethod
+from typing import Optional
 
 
 class SchedulerDriver(ABC):
     @abstractclassmethod
-    def array_batch_jobs(job_script: str, job_count: int, jobs_parallel: int = 1):
+    def array_batch_jobs(
+        job_script: str,
+        job_count: int,
+        jobs_parallel: int = 1,
+        array_dependency: Optional[str] = None,
+    ):
         pass
--- phdscripts/scheduler/slurm/driver.py
+++ phdscripts/scheduler/slurm/driver.py
@@ -3,16 +3,25 @@ Functions for interacting with Slurm scheduler.
 """
 
 from os import popen
+from typing import Optional
 
 from .. import SchedulerDriver
 
 
 class SlurmDriver(SchedulerDriver):
-    def array_batch_jobs(job_script: str, job_count: int, jobs_parallel: int = 1):
+    def array_batch_jobs(
+        job_script: str,
+        job_count: int,
+        jobs_parallel: int = 1,
+        array_dependency: Optional[str] = None,
+    ):
         cmd = f"sbatch --array=0-{job_count-1}"
 
         if jobs_parallel > 0:
             cmd += f"%{jobs_parallel}"
+
+        if array_dependency is not None:
+            cmd += f" --dependency=aftercorr:${array_dependency}"
 
         cmd += f" {job_script}"
 
