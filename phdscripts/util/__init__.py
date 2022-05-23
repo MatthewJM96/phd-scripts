f92bf0d03b17aefc3e729f946c6ef7b700476ceb
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-05-23 20:01:09 +0100

    Implement standardised replacements for fortran and decimnal numbers and create util submodule for them.

---
+++ phdscripts/util/__init__.py
@@ -0,0 +1,17 @@
+from .conversion import to_float
+from .string import (
+    convert_standard_to_fortran_number,
+    replace_decimal_number,
+    replace_decimal_numbers,
+    replace_fortran_number,
+    replace_fortran_numbers,
+)
+
+__all__ = [
+    "to_float",
+    "convert_standard_to_fortran_number",
+    "replace_decimal_number",
+    "replace_decimal_numbers",
+    "replace_fortran_number",
+    "replace_fortran_numbers",
+]
---
+++ phdscripts/util/string.py
@@ -0,0 +1,63 @@
+"""
+Utility functions for operating on strings.
+"""
+
+import re
+from typing import List
+
+__DECIMAL_NUMBER_PATTERN = r"[0-9]+[.[0-9]*]?"
+__FORTRAN_NUMBER_PATTERN = r"-?[0-9]+[.d\-?[0-9]*]?"
+__CAPTURED_STANDARD_NUMBER_PATTERN = r"(-?[0-9]+).?e(-?[0-9]+)"
+
+
+def convert_standard_to_fortran_number(target: str) -> str:
+    """
+    Replaces a standard notation number with an equivalent Fortran-compatible number
+    representation.
+    """
+    return re.sub(__CAPTURED_STANDARD_NUMBER_PATTERN, r"\1.d\2", target)
+
+
+def replace_decimal_number(pattern: str, sub: str, target: str) -> str:
+    """
+    Replace a decimal number found within a given pattern.
+    """
+
+    pattern = "(" + pattern.replace("@", f"){__DECIMAL_NUMBER_PATTERN}(", 1) + ")"
+
+    return re.sub(
+        pattern,
+        f"\1{sub}\2",
+        target,
+    )
+
+
+def replace_decimal_numbers(pattern: str, subs: List[str], target: str) -> str:
+    for sub in subs:
+        target = replace_decimal_number(pattern, sub, target)
+        pattern = pattern.replace("@", sub, 1)
+
+    return target
+
+
+def replace_fortran_number(pattern: str, sub: str, target: str) -> str:
+    """
+    Replaces a fortran number found within the given pattern.
+    """
+    # Place capture group around everything but the location to find a fortran number,
+    # at which point place the regex pattern.
+    pattern = "(" + pattern.replace("@", f"){__FORTRAN_NUMBER_PATTERN}(", 1) + ")"
+
+    return re.sub(
+        pattern,
+        f"\1{sub}\2",
+        target,
+    )
+
+
+def replace_fortran_numbers(pattern: str, subs: List[str], target: str) -> str:
+    for sub in subs:
+        target = replace_fortran_number(pattern, sub, target)
+        pattern = pattern.replace("@", sub, 1)
+
+    return target
