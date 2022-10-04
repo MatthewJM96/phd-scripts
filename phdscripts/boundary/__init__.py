c34b6e624b07d9b9483fe8a85c7c48fa60169568
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-10-04 16:11:54 +0100

    Export extrude_scale.

--- phdscripts/boundary/__init__.py
+++ phdscripts/boundary/__init__.py
@@ -1,4 +1,5 @@
 from .extrude_normal import extrude_normal
+from .extrude_scale import extrude_scale
 from .freidberg_cerfon import create_freidberg_cerfon_boundary
 
-__all__ = ["extrude_normal", "create_freidberg_cerfon_boundary"]
+__all__ = ["extrude_normal", "extrude_scale", "create_freidberg_cerfon_boundary"]
