9a95c356da23a6e877b2e1e1e2a1eb04fc5adf0c
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-10-21 11:29:18 +0100

    Add function to plot a set of profiles on the same plot with legend.

--- phdscripts/plotters/__init__.py
+++ phdscripts/plotters/__init__.py
@@ -1,3 +1,4 @@
+from .profiles import plot_profiles
 from .scatter_2d import scatter_2d
 
-__all__ = ["scatter_2d"]
+__all__ = ["plot_profiles", "scatter_2d"]
---
+++ phdscripts/plotters/profiles.py
@@ -0,0 +1,39 @@
+from typing import Callable, Dict, List, Optional, Tuple
+
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+
+def plot_profiles(
+    profiles: Dict[str, List[Tuple[float, float]]],
+    plot_method: Callable = plt.plot,
+    xaxis_name: Optional[str] = None,
+    yaxis_name: Optional[str] = None,
+    fig: Optional[Figure] = None,
+    output_file: Optional[str] = None,
+    show: bool = False,
+    aspect: str = "equal",
+):
+    plt.figure(fig)
+
+    if xaxis_name:
+        plt.xlabel(xaxis_name)
+
+    if yaxis_name:
+        plt.ylabel(yaxis_name)
+
+    for prof_name, prof_values in profiles.items():
+        (line,) = plot_method(
+            [value[0] for value in prof_values], [value[1] for value in prof_values]
+        )
+        line.set_label(prof_name)
+
+    if output_file or show:
+        plt.legend()
+
+    if output_file:
+        plt.savefig(output_file, transparent=True)
+
+    if show:
+        plt.gca().set_aspect(aspect, adjustable="box")
+        plt.show()
