e3881b255370b3a2ea6d0c6ab5da09427385bf2a
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-10-24 16:44:00 +0100

    Export helene and scene specific elite-to-jorek converters.

--- phdscripts/convert/__init__.py
+++ phdscripts/convert/__init__.py
@@ -1,3 +1,11 @@
-from elite_to_jorek import convert_elite_to_jorek
+from elite_to_jorek import (
+    convert_elite_to_jorek,
+    convert_helena_elite_to_jorek,
+    convert_scene_elite_to_jorek,
+)
 
-__all__ = ["convert_elite_to_jorek"]
+__all__ = [
+    "convert_elite_to_jorek",
+    "convert_helena_elite_to_jorek",
+    "convert_scene_elite_to_jorek",
+]
