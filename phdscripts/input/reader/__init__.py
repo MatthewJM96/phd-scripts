89ec305950618c364e0620847f9f0656376e9304
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-11-25 14:26:04 +0000

    Implement reader for JOREK equilibrium file.

--- phdscripts/input/reader/__init__.py
+++ phdscripts/input/reader/__init__.py
@@ -3,9 +3,12 @@ from .elite import (
     extract_from_helena_elite_input,
     extract_from_scene_elite_input,
 )
+from .jorek import read_equilibrium_file, read_input_profiles
 
 __all__ = [
     "extract_from_elite_input",
     "extract_from_helena_elite_input",
     "extract_from_scene_elite_input",
+    "read_equilibrium_file",
+    "read_input_profiles",
 ]
--- phdscripts/input/reader/jorek.py
+++ phdscripts/input/reader/jorek.py
@@ -1,5 +1,5 @@
 from os.path import isfile
-from typing import Dict, List, Tuple
+from typing import Callable, Dict, List, Tuple, Union
 
 EXPECTED_INPUT_PROFILE_HEADER = (
     '%"psin"       "FF\'"    "dFF\'/dpsin"    "rho"    "drho/dpsin"   "T"'
@@ -9,7 +9,7 @@ EXPECTED_INPUT_PROFILE_HEADER = (
 
 def __decompose_input_profiles(
     lines: List[str],
-) -> Dict[str, List[Tuple[float, float]]]:
+) -> Dict[str, Union[float, List[Tuple[float, float]]]]:
     profiles = {
         "ffprime": [],
         "dffprime/dpsi": [],
@@ -33,7 +33,7 @@ def __decompose_input_profiles(
 
 
 def __truncate_profiles_to_psi(
-    profiles: Dict[str, List[Tuple[float, float]]], truncate_at=1.0
+    profiles: Dict[str, Union[float, List[Tuple[float, float]]]], truncate_at=1.0
 ):
     for profile in profiles.values():
         for entry in reversed(profile):
@@ -43,7 +43,9 @@ def __truncate_profiles_to_psi(
             profile.pop()
 
 
-def read_input_profiles(filepath: str) -> Dict[str, List[Tuple[float, float]]]:
+def read_input_profiles(
+    filepath: str,
+) -> Dict[str, Union[float, List[Tuple[float, float]]]]:
     """
     Extracts the inpout profiles used by JOREK in a run. This is useful if any profile
     was specified using JOREK's functional form.
@@ -72,3 +74,170 @@ def read_input_profiles(filepath: str) -> Dict[str, List[Tuple[float, float]]]:
     __truncate_profiles_to_psi(profiles)
 
     return profiles
+
+
+def __extract_params(
+    names: List[Union[str, Tuple[str, Callable]]],
+    lines: List[str],
+    extracted_params: Dict[str, Union[float, List[Tuple[float, float]]]],
+) -> bool:
+    line = lines.pop(0)
+    values = line.strip().split()
+
+    if len(values) != len(names):
+        return False
+
+    try:
+        for name_idx in range(len(names)):
+            if type(names[name_idx]) is tuple:
+                extracted_params[names[name_idx][0]] = names[name_idx][1](
+                    values[name_idx]
+                )
+            else:
+                extracted_params[names[name_idx]] = float(values[name_idx])
+    except ValueError:
+        return False
+
+    return True
+
+
+def __extract_profile_params(
+    names: List[Union[str, Tuple[str, Callable]]],
+    lines: List[str],
+    extracted_params: Dict[str, Union[float, List[Tuple[float, float]]]],
+) -> bool:
+    profile_length = {}
+    if not __extract_params([("length", int)], lines, profile_length):
+        return False
+
+    profile_length = profile_length["length"]
+
+    for name_idx in range(len(names)):
+        if type(names[name_idx]) is tuple:
+            extracted_params[names[name_idx][0]] = [None] * profile_length
+        else:
+            extracted_params[names[name_idx]] = [None] * profile_length
+
+    for profile_idx in range(profile_length):
+        line = lines.pop(0)
+        values = line.strip().split()
+
+        if len(values) != len(names):
+            return False
+
+        try:
+            for name_idx in range(len(names)):
+                if type(names[name_idx]) is tuple:
+                    extracted_params[names[name_idx][0]][profile_idx] = names[name_idx][
+                        1
+                    ](values[name_idx])
+                else:
+                    extracted_params[names[name_idx]][profile_idx] = float(
+                        values[name_idx]
+                    )
+        except ValueError:
+            return False
+
+    return True
+
+
+def __extract_equilibrium_axis_params(
+    lines: List[str],
+    extracted_params: Dict[str, Union[float, List[Tuple[float, float]]]],
+) -> bool:
+    # Extract magnetic axis coords and F0.
+    if not __extract_params(
+        ["R_magnetic_axis", "Z_magnetic_axis", "F0"], lines, extracted_params
+    ):
+        return False
+
+    # Extract psi boundary and psi axis.
+    if not __extract_params(["psi_boundary", "psi_axis"], lines, extracted_params):
+        return False
+
+    return True
+
+
+def __extract_equilibrium_boundary(
+    lines: List[str],
+    extracted_params: Dict[str, Union[float, List[Tuple[float, float]]]],
+) -> bool:
+    # Extract boundary points.
+    if not __extract_profile_params(["R", "Z"], lines, extracted_params):
+        return False
+
+    return True
+
+
+def __extract_equilibrium_geometry_and_magnetics(
+    lines: List[str],
+    extracted_params: Dict[str, Union[float, List[Tuple[float, float]]]],
+) -> bool:
+    # Extract minor and major radius, and magnetic field on geometric axis.
+    if not __extract_params(
+        ["minor_radius", "major_radius", "magnetic_field_on_geometric_axis"],
+        lines,
+        extracted_params,
+    ):
+        return False
+
+    # Extract current, beta poloidal, and beta toroidal.
+    if not __extract_params(
+        ["current", "beta_poloidal", "beta_toroidal"], lines, extracted_params
+    ):
+        return False
+
+    # Extract beta normalised.
+    if not __extract_params(["beta_normalised"], lines, extracted_params):
+        return False
+
+    return True
+
+
+def __extract_equilibrium_profiles(
+    lines: List[str],
+    extracted_params: Dict[str, Union[float, List[Tuple[float, float]]]],
+) -> bool:
+    # Extract profiles.
+    if not __extract_profile_params(
+        ["psi", "pprime", "zjzprime", "q"], lines, extracted_params
+    ):
+        return False
+
+    return True
+
+
+def read_equilibrium_file(
+    filepath: str,
+) -> Dict[str, Union[float, List[Tuple[float, float]]]]:
+    """
+    Extracts the values stored in the equilibrium file generated by JOREK.
+    """
+
+    if not isfile(filepath):
+        print(f"JOREK equilibrium file does not exist:\n    {filepath}")
+        return False, {}
+
+    lines = []
+    with open(filepath, "r") as equilibrium_file:
+        lines = equilibrium_file.readlines()
+
+    if lines is None or (len(lines) == 1 and lines[0] == ""):
+        print(f"JOREK equilibrium file at:\n    {filepath}\nis empty!")
+        return False, {}
+
+    extracted_params = {}
+
+    if not __extract_equilibrium_axis_params(lines, extracted_params):
+        return {}
+
+    if not __extract_equilibrium_boundary(lines, extracted_params):
+        return {}
+
+    if not __extract_equilibrium_geometry_and_magnetics(lines, extracted_params):
+        return {}
+
+    if not __extract_equilibrium_profiles(lines, extracted_params):
+        return {}
+
+    return extracted_params
