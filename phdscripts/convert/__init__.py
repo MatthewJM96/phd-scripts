89658f57954fcb74f8c3fcf549ef54f5305cd988
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-11-25 15:16:17 +0000

    Fix export of convert_profile_points.

--- phdscripts/convert/__init__.py
+++ phdscripts/convert/__init__.py
@@ -1,10 +1,9 @@
-from profile_points import convert_profile_points
-
 from .elite_to_jorek import (
     convert_elite_to_jorek,
     convert_helena_elite_to_jorek,
     convert_scene_elite_to_jorek,
 )
+from .profile_points import convert_profile_points
 
 __all__ = [
     "convert_elite_to_jorek",
