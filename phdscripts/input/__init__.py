c7d1e1e33cb0ff63912075a07f4377aa01789b44
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2022-11-26 19:19:30 +0000

    Export input.reader input.writer in input.

---
+++ phdscripts/input/__init__.py
@@ -0,0 +1,23 @@
+from .reader import (
+    extract_from_elite_input,
+    extract_from_helena_elite_input,
+    extract_from_scene_elite_input,
+    read_jorek_equilibrium_file,
+    read_jorek_input_profile,
+    read_jorek_input_profiles,
+    read_jorek_namelist,
+)
+from .writer import write_helena_input, write_jorek_files, write_starwall_files
+
+__all__ = [
+    "extract_from_elite_input",
+    "extract_from_helena_elite_input",
+    "extract_from_scene_elite_input",
+    "read_jorek_equilibrium_file",
+    "read_jorek_input_profile",
+    "read_jorek_input_profiles",
+    "read_jorek_namelist",
+    "write_helena_input",
+    "write_jorek_files",
+    "write_starwall_files",
+]
