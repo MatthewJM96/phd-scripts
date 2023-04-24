67d384f95aaff7ffe965603b6601d06d8d64eb65
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2023-04-24 11:08:15 +0100

    Add conversion procedure to introduce cold boundary to a JOREK equilibrium.

--- phdscripts/convert/__init__.py
+++ phdscripts/convert/__init__.py
@@ -1,3 +1,4 @@
+from .add_cold_jorek_boundary import add_jorek_cold_boundary
 from .elite_to_jorek import (
     convert_elite_to_jorek,
     convert_helena_elite_to_jorek,
@@ -8,6 +9,7 @@ from .profile_points import convert_profile_points  # isort:skip
 from .jorek_to_helena import convert_jorek_to_helena  # isort:skip
 
 __all__ = [
+    "add_jorek_cold_boundary",
     "convert_elite_to_jorek",
     "convert_helena_elite_to_jorek",
     "convert_scene_elite_to_jorek",
