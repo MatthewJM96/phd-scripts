467cf2a348813e688281d0fd1cfc5dbc282d1963
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2023-04-24 11:07:42 +0100

    Allow specifying custom centre for extrude_scale, useful for specifying e.g. mag axis.

--- phdscripts/boundary/__init__.py
+++ phdscripts/boundary/__init__.py
@@ -1,5 +1,5 @@
 from .extrude_normal import extrude_normal
-from .extrude_scale import extrude_scale
+from .extrude_scale import extrude_scale, extrude_scale_from_centre
 from .fourier_decomp import (
     create_boundary_from_fourier_1d,
     create_boundary_from_fourier_2d,
@@ -12,6 +12,7 @@ from .print import print_starwall_wall_file
 __all__ = [
     "extrude_normal",
     "extrude_scale",
+    "extrude_scale_from_centre",
     "decomp_fourier_1d",
     "decomp_fourier_2d",
     "create_boundary_from_fourier_1d",
--- phdscripts/boundary/extrude_scale.py
+++ phdscripts/boundary/extrude_scale.py
@@ -2,7 +2,10 @@ from typing import List, Tuple
 
 
 def extrude_scale(
-    points: List[Tuple[float, float]], distance: float
+    points: List[Tuple[float, float]],
+    distance: float,
+    weighted_centre: bool = True,
+    scale_pow: float = 1.0,
 ) -> List[Tuple[float, float]]:
     """
     Scale up shape defined by the given points. This preserves shape and so does not
@@ -11,21 +14,63 @@ def extrude_scale(
     """
 
     centre = (0.0, 0.0)
-    for point in points:
-        centre = (centre[0] + point[0], centre[1] + point[1])
 
-    count = float(len(points))
-    centre = (centre[0] / count, centre[1] / count)
+    if weighted_centre:
+        for point in points:
+            centre = (centre[0] + point[0], centre[1] + point[1])
+
+        count = float(len(points))
+        centre = (centre[0] / count, centre[1] / count)
+    else:
+        R_min = 9999.0
+        R_max = 0.0
+        Z_min = 9999.0
+        Z_max = 0.0
+        for point in points:
+            if point[0] < R_min:
+                R_min = point[0]
+            elif point[0] > R_max:
+                R_max = point[0]
+            if point[1] < Z_min:
+                Z_min = point[1]
+            elif point[1] > Z_max:
+                Z_max = point[1]
+        centre = ((R_min + R_max) / 2.0, (Z_min + Z_max) / 2.0)
+
+    return extrude_scale_from_centre(points, distance, centre, scale_pow)
+
+
+def extrude_scale_from_centre(
+    points: List[Tuple[float, float]],
+    distance: float,
+    centre: Tuple[float, float],
+    scale_pow: float = 1.0,
+) -> List[Tuple[float, float]]:
+    """
+    Scale up shape defined by the given points and a specified centre point.
+    """
 
     extruded_points: List[Tuple[float, float]] = []
     for point in points:
+        print(point)
         origin_vec = (point[0] - centre[0], point[1] - centre[1])
 
-        extruded_points.append(
-            (
-                centre[0] + origin_vec[0] * (1.0 + distance),
-                centre[1] + origin_vec[1] * (1.0 + distance),
-            )
-        )
+        print(origin_vec)
+
+        extruded_pt_x = centre[0] + origin_vec[0]
+        if origin_vec[0] * distance >= 0.0:
+            extruded_pt_x += pow(abs(origin_vec[0] * distance), scale_pow)
+        else:
+            extruded_pt_x -= pow(abs(origin_vec[0] * distance), scale_pow)
+
+        extruded_pt_y = centre[1] + origin_vec[1]
+        if origin_vec[1] * distance >= 0.0:
+            extruded_pt_y += pow(abs(origin_vec[1] * distance), scale_pow)
+        else:
+            extruded_pt_y -= pow(abs(origin_vec[1] * distance), scale_pow)
+
+        extruded_points.append((extruded_pt_x, extruded_pt_y))
+
+        print(extruded_points[-1])
 
     return extruded_points
