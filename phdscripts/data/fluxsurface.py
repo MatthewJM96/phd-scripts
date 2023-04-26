951c65e6fec7df07b68ca0d7ca2a073771ec0639
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2023-04-26 15:19:20 +0100

    Implement adjust_boundary_psi_to_match_flux_surface.

--- phdscripts/data/fluxsurface.py
+++ phdscripts/data/fluxsurface.py
@@ -8,7 +8,11 @@ from typing import List, Tuple, Union
 from uuid import uuid4
 
 from phdscripts.boundary import create_boundary_from_fourier_2d, decomp_fourier_2d
-from phdscripts.input.reader import read_jorek_output, read_jorek_profile
+from phdscripts.input.reader import (
+    read_jorek_output,
+    read_jorek_profile,
+    read_jorek_RZpsi_profile,
+)
 
 REAL_PATTERN = r"-?[0-9]+.[0-9]+E[+-][0-9][0-9]"
 FLUXSURFACE_RESULTS_PATTERN = r"(" + REAL_PATTERN + r")\s+(" + REAL_PATTERN + r")"
@@ -309,6 +313,51 @@ def __adjust_boundary_RZ_to_match_flux_surface(
     return __smooth_points_fourier(new_boundary)
 
 
+def __adjust_boundary_psi_to_match_flux_surface(
+    origin: Tuple[float, float],
+    boundary: List[Tuple[float, float, float]],
+    actual_flux_surface: List[Tuple[float, float]],
+    target_flux_surface: List[Tuple[float, float]],
+    use_reciprocal_fact: bool,
+) -> List[Tuple[float, float]]:
+    """
+    Takes the actual flux surface in a JOREK run with the given JOREK boundary, and
+    computes a candidate boundary that will move the flux surface towards the target
+    flux surface.
+    """
+
+    new_boundary = []
+    for bnd_point in boundary:
+        theta = __theta(bnd_point[:2], origin)
+
+        actual_flux_at_theta = __interp_closest_points(
+            actual_flux_surface, theta, origin
+        )
+        target_flux_at_theta = __interp_closest_points(
+            target_flux_surface, theta, origin
+        )
+
+        actual_flux_mag_axis_dist = sqrt(
+            (actual_flux_at_theta[0] - origin[0]) ** 2.0
+            + (actual_flux_at_theta[1] - origin[1]) ** 2.0
+        )
+        target_flux_mag_axis_dist = sqrt(
+            (target_flux_at_theta[0] - origin[0]) ** 2.0
+            + (target_flux_at_theta[1] - origin[1]) ** 2.0
+        )
+
+        boundary_blowout = target_flux_mag_axis_dist / actual_flux_mag_axis_dist
+
+        if use_reciprocal_fact:
+            boundary_blowout = 1.0 / boundary_blowout
+
+        new_boundary.append(
+            (bnd_point[0], bnd_point[1], bnd_point[2] * boundary_blowout)
+        )
+
+    return __smooth_points_fourier(new_boundary)
+
+
 def adjust_boundary_RZ_to_match_flux_surface(
     psi: float,
     target_flux_surface: Union[str, List[Tuple[float, float]]],
@@ -382,4 +431,81 @@ def adjust_boundary_RZ_to_match_flux_surface(
 
     return True, __adjust_boundary_RZ_to_match_flux_surface(
         origin, boundary, actual_flux_surface, target_flux_surface
+    )
+
+
+def adjust_boundary_psi_to_match_flux_surface(
+    psi: float,
+    target_flux_surface: Union[str, List[Tuple[float, float]]],
+    jorek_postproc_binary: str,
+    jorek_directory: str,
+    jorek_output_filename: str,
+    jorek_namelist_filename: str = "jorek_namelist",
+    jorek_boundary_filename: str = "rz_boundary.txt",
+    use_geometric_axis: bool = False,
+    use_reciprocal_fact: bool = False,
+) -> Tuple[bool, List[Tuple[float, float]]]:
+    """
+    Takes the actual flux surface in a JOREK run with the given JOREK boundary, and
+    computes a candidate boundary that will move the flux surface towards the target
+    flux surface.
+    """
+
+    if psi < 0.0 or psi > 1.0:
+        print("Flux surface psi must be normalised.")
+        return False, []
+
+    success, actual_flux_surface = find_flux_surface(
+        psi, jorek_postproc_binary, jorek_directory, jorek_namelist_filename
+    )
+    if not success:
+        print("Could not obtain actual flux surface.")
+        return False, []
+
+    output_filepath = join(jorek_directory, jorek_output_filename)
+    success, output = read_jorek_output(output_filepath)
+    if not success:
+        print(f"Could not read JOREK output file: {output_filepath}")
+        return False, []
+
+    if use_geometric_axis:
+        if "geometric_centre" not in output.keys():
+            print(
+                "Could not extract needed information (mag axis) from JOREK std output."
+            )
+            return False, []
+
+        origin = (
+            float(output["geometric_centre"]["R"]),
+            float(output["geometric_centre"]["Z"]),
+        )
+    else:
+        if "magnetic_axis" not in output.keys():
+            print(
+                "Could not extract needed information (mag axis) from JOREK std output."
+            )
+            return False, []
+
+        origin = (
+            float(output["magnetic_axis"]["R"]),
+            float(output["magnetic_axis"]["Z"]),
+        )
+
+    boundary_filepath = join(jorek_directory, jorek_boundary_filename)
+    success, boundary = read_jorek_RZpsi_profile(boundary_filepath)
+    if not success:
+        print(f"Could not read JOREK boundary file: {boundary_filepath}")
+        return False, []
+
+    if isinstance(target_flux_surface, str):
+        flux_surface_file = target_flux_surface
+        success, target_flux_surface = read_jorek_profile(flux_surface_file)
+        if not success:
+            print(
+                f"Could not read target flux surface boundary file: {flux_surface_file}"
+            )
+            return False, []
+
+    return True, __adjust_boundary_psi_to_match_flux_surface(
+        origin, boundary, actual_flux_surface, target_flux_surface, use_reciprocal_fact
     )
