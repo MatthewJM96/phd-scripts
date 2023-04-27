5207d832328512e07286fafa4be29a9160c07915
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2023-04-27 11:52:36 +0100

    Export adjust_boundary_* from data.

--- phdscripts/data/__init__.py
+++ phdscripts/data/__init__.py
@@ -1,4 +1,15 @@
 from .boundary import BoundaryData, BoundaryType, MillerParameters
-from .fluxsurface import find_flux_surface
+from .fluxsurface import (
+    adjust_boundary_psi_to_match_flux_surface,
+    adjust_boundary_RZ_to_match_flux_surface,
+    find_flux_surface,
+)
 
-__all__ = ["BoundaryData", "BoundaryType", "MillerParameters", "find_flux_surface"]
+__all__ = [
+    "BoundaryData",
+    "BoundaryType",
+    "MillerParameters",
+    "find_flux_surface",
+    "adjust_boundary_psi_to_match_flux_surface",
+    "adjust_boundary_RZ_to_match_flux_surface",
+]
