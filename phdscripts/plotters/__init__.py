e7a3739e6bb3249786dc8860b0f5e17d5da0b0f4
Matthew Marshall <matthew.marshall@stfc.ac.uk>
2023-06-23 12:02:24 +0100

    Export geqdsk plotter.

--- phdscripts/plotters/__init__.py
+++ phdscripts/plotters/__init__.py
@@ -1,4 +1,5 @@
+from .geqdsk import plot_geqdsk
 from .profiles import plot_profiles
 from .scatter_2d import scatter_2d
 
-__all__ = ["plot_profiles", "scatter_2d"]
+__all__ = ["plot_geqdsk", "plot_profiles", "scatter_2d"]
