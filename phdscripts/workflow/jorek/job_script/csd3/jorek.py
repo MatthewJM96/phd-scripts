a11541d0ceed4361fb9c402297e56c853e725f3a
Matthew Marshall <matthew.marshall090396@gmail.com>
2023-07-31 14:35:50 +0100

    Rewrite job script writers for CSD3.

--- phdscripts/workflow/jorek/job_script/csd3/jorek.py
+++ phdscripts/workflow/jorek/job_script/csd3/jorek.py
@@ -13,22 +13,33 @@ def write_jorek_job_script(
     error_filename: str,
     log_name: str,
     walltime: str,
+    **kwargs,
 ):
+    # Set some defaults for nodes, CPUs per task, and number of tasks if these weren't
+    # provided.
+    if "nodes" not in kwargs:
+        kwargs = {**kwargs, "nodes": 2}
+    if "cpus_per_task" not in kwargs:
+        kwargs = {**kwargs, "cpus_per_task": 7}
+    if "ntasks" not in kwargs:
+        kwargs = {**kwargs, "ntasks": 8}
+
+    nodes = kwargs["nodes"]
+    ntasks = kwargs["ntasks"]
+    cpus_per_task = kwargs["cpus_per_task"]
+
     scheduler.write_array_job_script(
         job_script_filename,
         f"""
 ### Set environment
-source $HOME/.loaders/load_2017_env.sh
-source $HOME/.loaders/load_nov1_21_jorek.sh
+. /etc/profile.d/modules.sh
+source $HOME/load_jorek_modules.sh
 
-export OMP_NUM_THREADS=8
-export I_MPI_PIN_MODE=lib
+export OMP_NUM_THREADS={cpus_per_task}
+export I_MPI_PIN_DOMAIN=omp:compact
+export I_MPI_PIN_ORDER=scatter
 
-export KMP_AFFINITY=compact,verbose # These three lines were suggested by Tamas Feher
-export I_MPI_PIN_DOMAIN=auto        # and have been useful for decreasing computation
-export KMP_HW_SUBSET=1t             # time on KNL
-
-# Obtain working directory name from reigster.
+### Obtain working directory name from reigster.
 line_num=$((${{JOB_INDEX}} + 1))
 param_set="$(sed -n ${{line_num}}p {register})"
 IFS=',' read -ra param_set_parts <<< "$param_set"
@@ -36,17 +47,15 @@ param_set_name="${{param_set_parts[0]}}"
 
 cd {root_dir}/${{param_set_name}}
 
-mpirun -n 2                                \\
-    {jorek_exec} < {input_filename} \\
+mpirun -ppn {ntasks / nodes} -np {ntasks} \\
+    {jorek_exec} < {input_filename}       \\
         | tee log.{log_name}
         """,
         job_name=f"{run_id}_{log_name}",
-        partition="skl_fua_prod",
-        time=walltime,
-        nodes=2,
-        ntasks_per_node=1,
-        mem="177GB",
+        account="UKAEA-AP002-CPU",
+        partition="cclake",
         output=f"{root_dir}/%x.%a.{output_filename}",
         error=f"{root_dir}/%x.%a.{error_filename}",
-        account="FUA36_UKAEA_ML",
+        time=walltime,
+        **kwargs,
     )
--- phdscripts/workflow/jorek/job_script/csd3/starwall.py
+++ phdscripts/workflow/jorek/job_script/csd3/starwall.py
@@ -13,16 +13,31 @@ def write_starwall_job_script(
     error_filename: str,
     log_name: str,
     walltime: str,
+    **kwargs,
 ):
+    # Set some defaults for nodes, CPUs per task, and number of tasks if these weren't
+    # provided.
+    if "nodes" not in kwargs:
+        kwargs = {**kwargs, "nodes": 2}
+    if "cpus_per_task" not in kwargs:
+        kwargs = {**kwargs, "cpus_per_task": 1}
+    if "ntasks" not in kwargs:
+        kwargs = {**kwargs, "ntasks": 8}
+
+    nodes = kwargs["nodes"]
+    ntasks = kwargs["ntasks"]
+    cpus_per_task = kwargs["cpus_per_task"]
+
     scheduler.write_array_job_script(
         job_script_filename,
         f"""
 ### Set environment
-source $HOME/.loaders/load_2017_env.sh
-source $HOME/.loaders/load_nov1_21_jorek.sh
+. /etc/profile.d/modules.sh
+source $HOME/load_jorek_modules.sh
 
-export OMP_NUM_THREADS=1
-export I_MPI_PIN_MODE=lib
+export OMP_NUM_THREADS={cpus_per_task}
+export I_MPI_PIN_DOMAIN=omp:compact
+export I_MPI_PIN_ORDER=scatter
 
 # Obtain working directory name from reigster.
 line_num=$((${{JOB_INDEX}} + 1))
@@ -32,16 +47,15 @@ param_set_name="${{param_set_parts[0]}}"
 
 cd {root_dir}/${{param_set_name}}
 
-mpirun {starwall_exec} {input_filename} \\
+mpirun -ppn {ntasks / nodes} -np {ntasks} \\
+    {starwall_exec} {input_filename}      \\
         | tee log.{log_name}
             """,
         job_name=f"{run_id}_{log_name}",
-        partition="skl_fua_prod",
-        time=walltime,
-        nodes=1,
-        ntasks_per_node=48,
-        cpus_per_task=1,
+        account="UKAEA-AP002-CPU",
+        partition="cclake",
         output=f"{root_dir}/%x.%a.{output_filename}",
         error=f"{root_dir}/%x.%a.{error_filename}",
-        account="FUA36_UKAEA_ML",
+        time=walltime,
+        **kwargs,
     )
